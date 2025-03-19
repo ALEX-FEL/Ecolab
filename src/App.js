1c24a9ab18d64132750557d530be45d37381ad88
alex-felix <nengue382@gmail.com>
2025-03-19 03:34:39 +0100

    modification level

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./composants/Home";
 import Abonement from "./composants/abonement";
 import Contact from "./composants/contact";
@@ -9,28 +9,21 @@ import Detail_services from "./composants/services/detali";
 import Detail_abonmt from "./composants/abonnements/detail";
 import Visite_virtuelle from "./composants/services/visite_virtuelle";
 
-// Import des composantscontact
-
-
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Routes>
-          {/* Définir les routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/service" element={<Service />} /> 
-          <Route path="/ecoVirtual" element={<Detail_services />} /> 
-          <Route path="/abonement" element={<Abonement />} />
-          <Route path="/id_abonement" element={<Detail_abonmt />} />
-          <Route path="/visite_virtuelle" element={<Visite_virtuelle />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/ecoVirtual" element={<Detail_services />} />
+        <Route path="/abonement" element={<Abonement />} />
+        <Route path="/abonement/:id" element={<Detail_abonmt />} />
+        <Route path="/visite_virtuelle" element={<Visite_virtuelle />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
-
 
 export default App;
