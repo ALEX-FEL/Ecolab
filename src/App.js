a03b9cdc13dbf884ad39bad5cf7ebedb3e8d615b
ALEX-FEL <141037975+ALEX-FEL@users.noreply.github.com>
2025-10-20 08:22:11 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Abonement from "./composants/abonement";
 import Contact from "./composants/contact";
 import Service from "./composants/service";
 import About from "./composants/about";
-import Detail_services from "./composants/services/detali";
 import Detail_abonmt from "./composants/abonnements/detail";
+import JobsList from "./composants/jobs/JobsList";
+import JobDetail from "./composants/jobs/JobDetail";
+import Visite_virtuelle from "./composants/services/visite_virtuelle";
+import EcoVirtual_Lab from "./composants/services/EcoVirtual_Lab";
+import Formations from "./composants/services/formations";
 
 // Import des composantscontact
 
@@ -19,15 +23,20 @@ const App = () => {
           {/* Définir les routes */}
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/formations" element={<Formations />} />
           <Route path="/service" element={<Service />} /> 
-          <Route path="/id_service" element={<Detail_services />} /> 
+          <Route path="/ecoVirtual_lab" element={<EcoVirtual_Lab />} /> 
           <Route path="/abonement" element={<Abonement />} />
           <Route path="/id_abonement" element={<Detail_abonmt />} />
+          <Route path="/visite_virtuelle" element={<Visite_virtuelle />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/jobs" element={<JobsList />} />
+          <Route path="/jobs/:id" element={<JobDetail />} />
         </Routes>
       </div>
     </Router>
   );
 };
+
 
 export default App;
